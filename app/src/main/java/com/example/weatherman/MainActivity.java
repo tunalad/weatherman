9ea2d144148137c60e610f7be5ba71c4cf033cff
tunalad <novica.arsic@aol.com>
2022-05-20 01:32:17 +0200

    Designing part & refresh layout setup

--- app/src/main/java/com/example/weatherman/MainActivity.java
+++ app/src/main/java/com/example/weatherman/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.weatherman;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(MainActivity.this, "Weather updated", Toast.LENGTH_SHORT).show();
+                // update weather on refresh
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
 }
